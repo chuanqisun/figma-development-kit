a85918a1956b579e7e65e3958f3d0c60d468d9c9
Chuanqi <henrysun918@gmail.com>
2018-07-11 23:11:49 -0700

    update

---
+++ auth/figma-api.js
@@ -0,0 +1,68 @@
+const storageKeyForAuthorizationCodeData = 'figma-authorization-code-data';
+const storageKeyForAccessTokenData = 'figma-access-token-data';
+const authorizationEndpoint = 'https://www.figma.com/oauth';
+const accessTokenEndpoint = 'https://www.figma.com/api/oauth/token';
+
+export class FigmaApi {
+  constructor(config) {
+    if(!config) throw Error('config is required');
+    if(!config.clientId) throw Error('clientId is required');
+    if(!config.clientSecrete) throw Error('clientSecrete is required');
+    if(!config.redirectUri) throw Error('redirectUri is required');
+    this.config = config;
+  }
+
+  async getOAuth2Token() {
+    /* if a token exists and hasn't expired, re-use it */
+    const existingTokenData = JSON.parse(localStorage.getItem(storageKeyForAccessTokenData));
+    if (existingTokenData && existingTokenData.expireTimeInEpoch > Date.now()) {
+      return existingTokenData.token;
+    }
+  
+    /* if no token exists, request access code first */
+    const state = Math.random().toString(); // TODO randomize
+    
+    const tokenPromise = this.getAuthorizationCode(state)
+    .then(code => this.getAccessTokenData(code))
+    .then(accessTokenData => this.storeAccessTokenData(accessTokenData))
+    .catch(error => console.error(error));
+   
+    window.open(`${authorizationEndpoint}?client_id=${this.config.clientId}&redirect_uri=${this.config.redirectUri}&scope=file_read&state=${state}&response_type=code`);
+  
+    return tokenPromise;
+  }
+  
+  async getAuthorizationCode(trueState) {
+    return new Promise((resolve, reject) => {
+      let storageEventHandler = null;
+      window.addEventListener('storage', storageEventHandler = event => {
+        if (event.key === storageKeyForAuthorizationCodeData) {
+          const {code, state} = JSON.parse(localStorage.getItem(storageKeyForAuthorizationCodeData));
+          window.removeEventListener('storage', storageEventHandler);
+          localStorage.removeItem(storageKeyForAuthorizationCodeData);
+          if (state !== trueState) {
+            reject('STATE_MISMATCH');
+          } else {
+            resolve(code);
+          }
+        }
+      });
+    });
+  }
+  
+  async getAccessTokenData(authorizationCode) {
+    return fetch(`${accessTokenEndpoint}?client_id=${this.config.clientId}&client_secret=${this.config.clientSecrete}&redirect_uri=${this.config.redirectUri}&code=${authorizationCode}&grant_type=authorization_code`, {
+      method: 'POST',
+    }).then(response => response.json())
+    .then(responseObject => {
+      const {access_token, expires_in} = responseObject;
+      const expireTimeInEpoch = Date.now() + expires_in;
+      return {token: access_token, expireTimeInEpoch};
+    });
+  }
+  
+  async storeAccessTokenData(accessTokenData) {
+    localStorage.setItem(storageKeyForAccessTokenData, JSON.stringify(accessTokenData));
+    return accessTokenData.token;
+  }
+}
--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-import { getOAuth2Token } from './auth/figma-auth.js';
+import { FigmaApi } from './auth/figma-api.js';
 
-document.querySelector('.js-login-button').onclick = () => getOAuth2Token(token => {
+const fiamgaApi = new FigmaApi({
+  clientId: 'd6HPfFabvUgC73Y9MSsRy6',
+  clientSecrete: 'U7zKxVvYBqoOJD9tH7pFPUEveqjK7X',
+  redirectUri: 'http://localhost:5500/callback.html',
+});
+
+document.querySelector('.js-login-button').onclick = () => fiamgaApi.getOAuth2Token().then(token => {
   console.dir(token);
 });
